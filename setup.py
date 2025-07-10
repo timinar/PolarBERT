b767d4d342af7a6ddf4c3aca325d82afa960cdab
Jean-Loup Tastet <jeta@di.ku.dk>
2025-07-10 15:58:22 +0200

    Move setup to pyproject.toml and exclude more whitespace linter rules

--- setup.py
+++ setup.py
@@ -1,31 +1,4 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-setup(
-    name="polarbert",
-    version="0.1.0",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    install_requires=[
-        "torch>=2.0.0",
-        "pytorch-lightning>=2.0.0",
-        "numpy",
-        "pandas",
-        "pyarrow",
-        "tqdm",
-        "pyyaml",
-        "wandb",
-    ],
-    author="Inar Timiryasov",
-    author_email="timinar@gmail.com",
-    description="PolarBERT - A foundation model for IceCube",
-    long_description=open("README.md").read() if os.path.exists("README.md") else "",
-    long_description_content_type="text/markdown",
-    url="https://github.com/timinar/PolarBERT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.10",
-)
+# This will automatically use the configuration from pyproject.toml
+setup()
